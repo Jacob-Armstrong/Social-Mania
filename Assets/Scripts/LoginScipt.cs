b8bf3e9becad79707c5ecd76b1505db80e186267
LucasDNguyen <LucasDuyNguyen@gmail.com>
2023-02-23 18:35:59 -0800

    Created Scripts
    Created blank scripts for each button on login page

---
+++ Assets/Scripts/LoginScipt.cs
@@ -0,0 +1,19 @@
+using LootLocker.Requests;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginScipt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
