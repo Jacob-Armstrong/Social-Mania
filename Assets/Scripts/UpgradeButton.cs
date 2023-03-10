d18a9313aac704823f675cf02da70b23989ab2d4
jayrock3000 <jeffrhoten@gmail.com>
2023-03-09 18:40:07 -0800

    hulk rogan

---
+++ Assets/Scripts/UpgradeButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour
+{
+    [SerializeField]
+    private Image myIcon;
+
+    public void SetIcon(Sprite mySprite)
+    {
+        myIcon.sprite = mySprite
+    }
+}
