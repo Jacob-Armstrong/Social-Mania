647fdb9fd48fa53de9357cc7a409cefd6c0c1758
jayrock3000 <jeffrhoten@gmail.com>
2023-04-25 13:31:58 -0700

    FinishedNewsFeed

--- Assets/Scripts/NewsFeed/NewsFeedDisplay.cs
+++ Assets/Scripts/NewsFeed/NewsFeedDisplay.cs
@@ -8,11 +8,6 @@ public class NewsFeedDisplay : MonoBehaviour
     [SerializeField]
     private GameObject newsTemplate;
 
-    //[SerializeField]
-    //private TextAsset textFile;
-
-
-    //private List<GameObject> newsStoryList = new List<GameObject>();
     public List<NewsStory> newsStoryList = new List<NewsStory>();
 
     private float repeatDelay = 20.0f;
@@ -34,6 +29,12 @@ public class NewsFeedDisplay : MonoBehaviour
 
         username = "[name unknown]";
 
+
+
+        // Max Length:
+        // content: BREAKING NEWS: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        // eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam dol
+
         content = "New social media influencer #USERNAME# starts online career. " +
             "\"Is this really news?\" asks expert.";
         minViews = 0.0f;
@@ -41,8 +42,7 @@ public class NewsFeedDisplay : MonoBehaviour
         minAttention = 0;
         NewsStory s1 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s1);
-
-        
+      
         content = "New study reports over ten thousand videos uploaded to the internet per second " +
             "\"I don't care, I'm going to make it!\" says moron.";
         minViews = 0.0f;
@@ -58,126 +58,147 @@ public class NewsFeedDisplay : MonoBehaviour
         NewsStory s3 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s3);
 
-        content = "Massively popular news website claims it has made news tickers obsolete. " +
-            "In other news, massively popular news website is wrong and stupid.";
+        content = "New trend of content creators uploading silent videos to honor the deaf " +
+            "outrages the blind.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s4 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s4);
         
-        content = "blank";
+        content = "BREAKING NEWS: \"Everything is fine\" claims CEO standing in front of " +
+            "burning building.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s5 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s5);
 
-        content = "blank";
+        content = "\"Artificial intelligence is not a problem, just carry on\" says real human.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s6 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s6);
 
-        content = "blank";
+        content = "\"Whoever said that news stories are being written by artificial intelligence " +
+            "is a dingus\" says bingus.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s7 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s7);
 
-        content = "blank";
+        content = "BREAKING NEWS: Rioters smash like button. \"We can never put it back " +
+            "together again,\" says Chief of Internet Police.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s8 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s8);
 
-        content = "blank";
+        content = "New cleaning trend sweeps the nation. \"Well at least it's not something " +
+            "pointless like social media,\" says mother.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s9 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s9);
 
-        content = "blank";
+        content = "BREAKING NEWS: Podcast about cancel culture cancelled. \"I don't know what " +
+            "we expected.\"";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s10 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s10);
 
-        content = "blank";
+        content = "Health claims about new online diet disputed by out of touch parents. " +
+            "\"Potato chips won't cure influenza!\"";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s11 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s11);
 
-        content = "blank";
+        content = "TODAY: Outrage towards crypto CEO who defrauded investors. \"I learned " +
+            "my lesson, and to make ammends I am proud to announce the new IMSORRY token...\"";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s12 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s12);
 
-        content = "blank";
+        content = "Social media influencer Clevus McBevus arrested after multi-hour shootout " +
+            "with police. \"It was just a prank, bro.\"";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s13 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s13);
 
-        content = "blank";
+        content = "THIS JUST IN: Celebrities release video song about saving the world. " +
+            "Moments later, all world problems were solved.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s14 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s14);
 
-        content = "blank";
+        content = "TONIGHT AT 11: Interview with controversial creator of content for children. " +
+            "\"It's not for the views, I just really like Elsa and Spiderman.\"";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s15 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s15);
 
-        content = "blank";
+        content = "\"This isn't about money\" says highest paid content creator on the platform.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s16 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s16);
 
-        content = "blank";
+        content = "TODAY: Debate whether corporate money influences news media. \"This is " +
+            "preposterous!\" claims CEO of company that makes delicious yet affordable potato chips.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s17 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s17);
 
-        content = "blank";
+        content = "\"Traditional news media no longer relevant,\" says person who is wrong " +
+            "and should shut up.";
         minViews = 0.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s18 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s18);
 
-        content = "blank";
-        minViews = 0.0f;
+        content = "Social media influencer #USERNAME# growing in popularity. \"I've never " +
+            "been so proud to be disappointed in them,\" says their mother.";
+        minViews = 10000.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s19 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s19);
 
-        content = "blank";
-        minViews = 0.0f;
+        content = "#USERNAME# now has millions of views. \"Isn't there a war going on?\" " +
+            "asks out of touch citizen.";
+        minViews = 1000000.0f;
         minFollowers = 0;
         minAttention = 0;
         NewsStory s20 = new NewsStory(content, minViews, minFollowers, minAttention);
         newsStoryList.Add(s20);
-        
+
+        content = "Social media empire of #USERNAME# growing out of control. Countless fans " +
+            "call for them to run for president or upload videos more regularly.";
+        minViews = 1000000000.0f;
+        minFollowers = 0;
+        minAttention = 0;
+        NewsStory s21 = new NewsStory(content, minViews, minFollowers, minAttention);
+        newsStoryList.Add(s21);
 
 
 
@@ -211,7 +232,7 @@ public class NewsFeedDisplay : MonoBehaviour
         // Replace text with original (necessary for username to change dynamically)
         newsStoryList[storyID].content = originalText;
 
-        Debug.Log("Current views is actually: " + resourceObject.GetComponent<Resources>().views);
+        //Debug.Log("Current views is actually: " + resourceObject.GetComponent<Resources>().views);
 
         // Delete object after a delay of several seconds
         Destroy(newText, repeatDelay);
@@ -221,7 +242,7 @@ public class NewsFeedDisplay : MonoBehaviour
     {
         // Select a random news story
         int randomID = Random.Range(1, newsStoryList.Count + 1) - 1;
-        Debug.Log("Selected number: " + randomID);
+        //Debug.Log("Selected number: " + randomID);
 
         // Check if the requirements are met
         if (NewsRequirementsMet(newsStoryList[randomID]) == true) {
@@ -241,6 +262,7 @@ public class NewsFeedDisplay : MonoBehaviour
     // next, determine if displaying is valid
     bool NewsRequirementsMet(NewsStory thisStory)
     {
+        // Get views from resources script
         double views = resourceObject.GetComponent<Resources>().views;
 
         if (views >= thisStory.minViews)
@@ -254,9 +276,7 @@ public class NewsFeedDisplay : MonoBehaviour
         }
     }
 
-    // else, choose another story
-    // and display the story
-
+    // Class to hold content and minimum values
     public class NewsStory
     {
         public string content;
